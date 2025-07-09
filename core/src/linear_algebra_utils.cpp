d4c3ee7741f30662a430def8740c950814a34abd
Brad Ross <brad.ross.35@gmail.com>
2025-07-08 17:25:00 -0700

    fixed bug to handle rank-deficient matrices.

--- core/src/linear_algebra_utils.cpp
+++ core/src/linear_algebra_utils.cpp
@@ -11,7 +11,18 @@ arma::mat projection_matrix(const arma::mat& X) {
     arma::vec s;
     arma::svd_econ(U, s, V, X, "left");
 
-    return U * U.t();
+    // Determine the rank by finding the number of singular values greater than a tolerance.
+    double tol = std::max(X.n_rows, X.n_cols) * s.max() * arma::Datum<double>::eps;
+    arma::uword r = arma::sum(s > tol);
+
+    if (r == 0) {
+        return arma::mat(X.n_rows, X.n_rows, arma::fill::zeros);
+    }
+
+    // Use only the first 'r' columns of U, which form a basis for the column space.
+    arma::mat U_r = U.cols(0, r - 1);
+
+    return U_r * U_r.t();
 }
 
 } // namespace apm 
