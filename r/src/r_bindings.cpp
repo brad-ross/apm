5793e54809d0f62930fd23bfd220c4218c8ffc24
Brad Ross <brad.ross.35@gmail.com>
2025-07-10 11:22:50 -0700

    updated R bindings for new functions

--- r/src/r_bindings.cpp
+++ r/src/r_bindings.cpp
@@ -47,44 +47,76 @@ arma::mat align_factors_using_apm(
 
 //' Impute outcomes for a representative unit
 //'
+//' This function supports various combinations of factors, fixed effects, and covariates.
+//'
 //' @param G A T x r matrix of factors.
-//' @param a A q-dimensional vector of covariate coefficients.
 //' @param T_c A vector of 1-based indices for the observed time periods for the cohort.
 //' @param m_c A vector containing the observed outcomes for the representative unit.
-//' @param X_c A T x q matrix containing the values of q covariates corresponding to each outcome.
+//' @param g_0 An optional T-dimensional vector of estimated outcome fixed effects.
+//' @param a An optional q-dimensional vector of estimated covariate coefficients.
+//' @param X_c An optional T x q matrix of covariates. Required if 'a' is provided.
 //' @return A T-dimensional vector containing the estimated outcomes for the representative unit.
 //' @export
 // [[Rcpp::export]]
 arma::vec impute_outcomes(
     const arma::mat& G,
-    const arma::vec& a,
     const arma::uvec& T_c,
     const arma::vec& m_c,
-    const arma::mat& X_c) {
+    Rcpp::Nullable<Rcpp::NumericVector> g_0 = R_NilValue,
+    Rcpp::Nullable<Rcpp::NumericVector> a = R_NilValue,
+    Rcpp::Nullable<Rcpp::NumericMatrix> X_c = R_NilValue) {
     
-    // R is 1-based, C++ is 0-based.
-    return apm::impute_outcomes(G, a, T_c - 1, m_c, X_c);
+    arma::uvec T_c_zero_based = T_c - 1;
+
+    bool has_g0 = g_0.isNotNull();
+    bool has_a = a.isNotNull();
+
+    if (has_a && !X_c.isNotNull()) {
+        Rcpp::stop("If 'a' is provided, 'X_c' must also be provided.");
+    }
+    if (!has_a && X_c.isNotNull()) {
+        Rcpp::warning("'X_c' is provided but 'a' is not; covariates will be ignored.");
+    }
+
+    if (has_g0 && has_a) {
+        arma::vec g_0_cpp = Rcpp::as<arma::vec>(g_0);
+        arma::vec a_cpp = Rcpp::as<arma::vec>(a);
+        arma::mat X_c_cpp = Rcpp::as<arma::mat>(X_c);
+        return apm::impute_outcomes(G, g_0_cpp, a_cpp, T_c_zero_based, m_c, X_c_cpp);
+    } else if (has_g0) {
+        arma::vec g_0_cpp = Rcpp::as<arma::vec>(g_0);
+        return apm::impute_outcomes(G, g_0_cpp, T_c_zero_based, m_c);
+    } else if (has_a) {
+        arma::vec a_cpp = Rcpp::as<arma::vec>(a);
+        arma::mat X_c_cpp = Rcpp::as<arma::mat>(X_c);
+        return apm::impute_outcomes(G, a_cpp, T_c_zero_based, m_c, X_c_cpp);
+    } else {
+        return apm::impute_outcomes(G, T_c_zero_based, m_c);
+    }
 }
 
 //' Estimate mean outcomes across cohorts
 //'
+//' This function supports various combinations of factors, fixed effects, and covariates.
+//'
 //' @param G A T x r matrix whose rows are estimated factor vectors.
-//' @param a A q-dimensional vector of estimated covariate coefficients.
 //' @param observed_outcome_indices A list where each element is a vector of 
 //'                                 1-based indices for the observed time periods for a cohort.
 //' @param m_c_vec A list of arma::vec, where each vector m_c contains the 
 //'                observed outcomes for a cohort.
-//' @param X_c_vec A list of T x q matrices, where each matrix X_c contains 
-//'                the average values of q covariates for each outcome within a cohort.
+//' @param g_0 An optional T-dimensional vector of estimated outcome fixed effects.
+//' @param a An optional q-dimensional vector of estimated covariate coefficients.
+//' @param X_c_vec An optional list of T x q matrices of covariates. Required if 'a' is provided.
 //' @return A C x T matrix where each row c contains the estimated T mean outcomes for cohort c.
 //' @export
 // [[Rcpp::export]]
 arma::mat estimate_outcome_means_across_cohorts(
     const arma::mat& G,
-    const arma::vec& a,
     Rcpp::List observed_outcome_indices,
     Rcpp::List m_c_vec,
-    Rcpp::List X_c_vec) {
+    Rcpp::Nullable<Rcpp::NumericVector> g_0 = R_NilValue,
+    Rcpp::Nullable<Rcpp::NumericVector> a = R_NilValue,
+    Rcpp::Nullable<Rcpp::List> X_c_vec = R_NilValue) {
 
     std::vector<arma::uvec> cpp_observed_indices;
     for (SEXP vec : observed_outcome_indices) {
@@ -97,10 +129,37 @@ arma::mat estimate_outcome_means_across_cohorts(
         cpp_m_c_vec.push_back(Rcpp::as<arma::vec>(vec));
     }
 
-    std::vector<arma::mat> cpp_X_c_vec;
-    for (SEXP mat : X_c_vec) {
-        cpp_X_c_vec.push_back(Rcpp::as<arma::mat>(mat));
+    bool has_g0 = g_0.isNotNull();
+    bool has_a = a.isNotNull();
+
+    if (has_a && !X_c_vec.isNotNull()) {
+        Rcpp::stop("If 'a' is provided, 'X_c_vec' must also be provided.");
     }
-    
-    return apm::estimate_outcome_means_across_cohorts(G, a, cpp_observed_indices, cpp_m_c_vec, cpp_X_c_vec);
+     if (!has_a && X_c_vec.isNotNull()) {
+        Rcpp::stop("If 'X_c_vec' is provided, 'a' must also be provided.");
+    }
+
+    if (has_g0 && has_a) {
+        arma::vec g_0_cpp = Rcpp::as<arma::vec>(g_0);
+        arma::vec a_cpp = Rcpp::as<arma::vec>(a);
+        std::vector<arma::mat> cpp_X_c_vec;
+        Rcpp::List r_X_c_vec(X_c_vec);
+        for (SEXP mat : r_X_c_vec) {
+            cpp_X_c_vec.push_back(Rcpp::as<arma::mat>(mat));
+        }
+        return apm::estimate_outcome_means_across_cohorts(G, g_0_cpp, a_cpp, cpp_observed_indices, cpp_m_c_vec, cpp_X_c_vec);
+    } else if (has_g0) {
+        arma::vec g_0_cpp = Rcpp::as<arma::vec>(g_0);
+        return apm::estimate_outcome_means_across_cohorts(G, g_0_cpp, cpp_observed_indices, cpp_m_c_vec);
+    } else if (has_a) {
+        arma::vec a_cpp = Rcpp::as<arma::vec>(a);
+        std::vector<arma::mat> cpp_X_c_vec;
+        Rcpp::List r_X_c_vec(X_c_vec);
+        for (SEXP mat : r_X_c_vec) {
+            cpp_X_c_vec.push_back(Rcpp::as<arma::mat>(mat));
+        }
+        return apm::estimate_outcome_means_across_cohorts(G, a_cpp, cpp_observed_indices, cpp_m_c_vec, cpp_X_c_vec);
+    } else {
+        return apm::estimate_outcome_means_across_cohorts(G, cpp_observed_indices, cpp_m_c_vec);
+    }
 } 
--- r/tests/testthat/test-apm.R
+++ r/tests/testthat/test-apm.R
@@ -120,57 +120,92 @@ test_that("APM alignment works for a non-contiguous pattern", {
   run_alignment_test_r(true_factors, observed_outcome_indices)
 }) 
 
-test_that("Outcome estimation and imputation functions work correctly", {
-  Tval <- 4
-  r <- 2
-  Cval <- 2
-  q <- 2
+# Helper function to set up data for estimation tests
+setup_estimation_test_data_r <- function() {
+  list(
+    Tval = 4, r = 2, Cval = 2, q = 2,
+    G = matrix(c(1, 1, 2, 4, 3, 9, 4, 16), nrow = 4, ncol = 2, byrow = TRUE),
+    a = c(0.5, 1.0),
+    g_0 = seq(0.1, 0.4, by = 0.1),
+    observed_outcome_indices = list(c(1, 2, 3), c(2, 3, 4)),
+    l_c = list(c(1.0, 2.0), c(3.0, 4.0)),
+    X_c_vec = list(
+      matrix(c(0.1, 0.5, 0.2, 0.6, 0.3, 0.7, 0.4, 0.8), nrow = 4, ncol = 2, byrow = TRUE),
+      matrix(c(1.1, 1.5, 1.2, 1.6, 1.3, 1.7, 1.4, 1.8), nrow = 4, ncol = 2, byrow = TRUE)
+    )
+  )
+}
+
+test_that("EstimateMeans_FactorsCovariatesFixedEffects", {
+  data <- setup_estimation_test_data_r()
   
-  # Note: R uses 1-based indexing
-  observed_outcome_indices <- list(c(1, 2, 3), c(2, 3, 4))
-  
-  G <- matrix(c(1, 1, 2, 4, 3, 9, 4, 16), nrow = Tval, ncol = r, byrow = TRUE)
-  a <- c(0.5, 1.0)
-  
-  l_c <- list(c(1.0, 2.0), c(3.0, 4.0))
-  
-  X1 <- matrix(c(
-    0.1, 0.5,
-    0.2, 0.6,
-    0.3, 0.7,
-    0.4, 0.8
-  ), nrow = Tval, ncol = q, byrow = TRUE)
-  
-  X2 <- matrix(c(
-    1.1, 1.5,
-    1.2, 1.6,
-    1.3, 1.7,
-    1.4, 1.8
-  ), nrow = Tval, ncol = q, byrow = TRUE)
-  
-  X_c_vec <- list(X1, X2)
-  
-  # Calculate true means
-  m <- matrix(0, nrow = Cval, ncol = Tval)
-  m[1, ] <- (G %*% l_c[[1]] + X1 %*% a)
-  m[2, ] <- (G %*% l_c[[2]] + X2 %*% a)
+  true_m <- matrix(0, nrow = data$Cval, ncol = data$Tval)
+  true_m[1,] <- (data$G %*% data$l_c[[1]] + data$X_c_vec[[1]] %*% data$a + data$g_0)
+  true_m[2,] <- (data$G %*% data$l_c[[2]] + data$X_c_vec[[2]] %*% data$a + data$g_0)
   
   m_c_vec <- list(
-    m[1, observed_outcome_indices[[1]]],
-    m[2, observed_outcome_indices[[2]]]
+    true_m[1, data$observed_outcome_indices[[1]]],
+    true_m[2, data$observed_outcome_indices[[2]]]
   )
   
-  # Test estimate_outcome_means_across_cohorts
   estimated_m <- estimate_outcome_means_across_cohorts(
-    G, a, observed_outcome_indices, m_c_vec, X_c_vec
+    data$G, data$observed_outcome_indices, m_c_vec, 
+    g_0 = data$g_0, a = data$a, X_c_vec = data$X_c_vec)
+  
+  expect_equal(estimated_m, true_m, tolerance = 1e-9)
+})
+
+test_that("EstimateMeans_FactorsAndFixedEffects", {
+  data <- setup_estimation_test_data_r()
+  
+  true_m <- matrix(0, nrow = data$Cval, ncol = data$Tval)
+  true_m[1,] <- (data$G %*% data$l_c[[1]] + data$g_0)
+  true_m[2,] <- (data$G %*% data$l_c[[2]] + data$g_0)
+  
+  m_c_vec <- list(
+    true_m[1, data$observed_outcome_indices[[1]]],
+    true_m[2, data$observed_outcome_indices[[2]]]
   )
   
-  expect_equal(estimated_m, m, tolerance = 1e-9)
+  estimated_m <- estimate_outcome_means_across_cohorts(
+    data$G, data$observed_outcome_indices, m_c_vec, g_0 = data$g_0)
   
-  # Test impute_outcomes using the first cohort's data
-  estimated_m_cohort1 <- impute_outcomes(
-    G, a, observed_outcome_indices[[1]], m_c_vec[[1]], X_c_vec[[1]]
+  expect_equal(estimated_m, true_m, tolerance = 1e-9)
+})
+
+test_that("EstimateMeans_FactorsAndCovariates", {
+  data <- setup_estimation_test_data_r()
+  
+  true_m <- matrix(0, nrow = data$Cval, ncol = data$Tval)
+  true_m[1,] <- (data$G %*% data$l_c[[1]] + data$X_c_vec[[1]] %*% data$a)
+  true_m[2,] <- (data$G %*% data$l_c[[2]] + data$X_c_vec[[2]] %*% data$a)
+  
+  m_c_vec <- list(
+    true_m[1, data$observed_outcome_indices[[1]]],
+    true_m[2, data$observed_outcome_indices[[2]]]
   )
   
-  expect_equal(as.vector(estimated_m_cohort1), m[1, ], tolerance = 1e-9)
+  estimated_m <- estimate_outcome_means_across_cohorts(
+    data$G, data$observed_outcome_indices, m_c_vec, 
+    a = data$a, X_c_vec = data$X_c_vec)
+  
+  expect_equal(estimated_m, true_m, tolerance = 1e-9)
+})
+
+test_that("EstimateMeans_FactorsOnly", {
+  data <- setup_estimation_test_data_r()
+  
+  true_m <- matrix(0, nrow = data$Cval, ncol = data$Tval)
+  true_m[1,] <- (data$G %*% data$l_c[[1]])
+  true_m[2,] <- (data$G %*% data$l_c[[2]])
+  
+  m_c_vec <- list(
+    true_m[1, data$observed_outcome_indices[[1]]],
+    true_m[2, data$observed_outcome_indices[[2]]]
+  )
+  
+  estimated_m <- estimate_outcome_means_across_cohorts(
+    data$G, data$observed_outcome_indices, m_c_vec)
+  
+  expect_equal(estimated_m, true_m, tolerance = 1e-9)
 }) 
